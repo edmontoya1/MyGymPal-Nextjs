99f97ce4a57558974e1888048e2d3c66554db886
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2023-05-16 11:15:59 -0500

    [New]: Home page

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Loading = (props: Props) => {
+  return <div className="w-screen bg-white">loading...</div>;
+};
+
+export default Loading;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Home
+    <main className="flex min-h-screen flex-col items-center justify-around text-white">
+      <div className="w-80 rounded-lg bg-[#a9a9a9] p-5">
+        <h2>MyGymPal</h2>
+        <h3>CSCI-4830</h3>
+        <h2>Introduction To Software Engineering</h2>
+      </div>
+      <div className="flex ">
+        <div className="m-2 w-32 rounded-md bg-[#a9a9a9] p-2 text-center">
+          <Link href={"/users"}>All Users</Link>
+        </div>
+        <div className="m-2 w-32 rounded-md bg-[#a9a9a9] p-2 text-center">
+          <Link href={"/sessions"}>All Sessions</Link>
+        </div>
+      </div>
     </main>
   );
 }
